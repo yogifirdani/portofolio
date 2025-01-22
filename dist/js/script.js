e1f175a480cbf5481e67128bccbec101de81c0ff
yogifirdani <yogifirdani02@gmail.com>
2025-01-22 09:08:56 +0700

    portofolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+// navbar fixed fo scroll fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+const humberger = document.querySelector("#humberger");
+const navMenu = document.querySelector("#nav-menu");
+
+// humberger
+humberger.addEventListener("click", function () {
+  humberger.classList.toggle("humberger-active");
+  navMenu.classList.toggle("hidden");
+});
